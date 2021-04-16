8264d254018668cd830a9e8d63fd00f68f6df62b
Allyson English <58833140+Allyson-English@users.noreply.github.com>
2021-04-16 08:46:28 -0400

    AI player finished but unintelligent, as yet

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -53,10 +54,12 @@ def actions(board):
     return options
 
 
-def result(board, action, user):
+def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    
+    user = player(board)
     
     x, y = action
     board[x][y] = user
@@ -108,11 +111,61 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    
+    if winner(board) == 'X':
+        return 1
+    if winner(board) == 'O':
+        return -1
+    if winner(board) == None:
+        return 0
+    
+    return None
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    
+def minimax(board):
+    
+    # determine active player
+    active_player = player(board)
+    
+    # if max player is active 
+    if active_player == 'X':
+        
+        # make a deep copy of board state 
+        cpy = copy.deepcopy(board)
+        
+        # display available options
+        options = actions(cpy)
+        
+        while not winner(cpy):
+        
+            # for each option 
+            for mv in options:
+            
+                if utility(result(cpy, mv)) != -1:
+                    return mv
+            
+        
+    if active_player == 'O':
+        
+        # make a deep copy of board state 
+        cpy = copy.deepcopy(board)
+        
+        # display available options
+        options = actions(cpy)
+        
+        while not winner(cpy):
+        
+            # for each option 
+            for mv in options:
+            
+                if utility(result(cpy, mv)) != 1:
+                    return mv
+            
+                else:
+                    temp = result(cpy, mv)
+                    mnmx = minimax(temp)
